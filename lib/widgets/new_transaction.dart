13a103bfd75a4ed8ab8937868d8db85e2fe2947b
amira334 <amira35-334@diu.edu.bd>
2023-01-30 14:52:52 +0600

    implement "widget lifecycle in new_transaction file"

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -7,16 +7,44 @@ import '../widgets/adaptive_flat_button.dart';
 class NewTransaction extends StatefulWidget {
   final Function addTr;
 
-  NewTransaction(this.addTr);
+  NewTransaction(this.addTr){
+    print('Constructor NewTransaction widget');
+  }
 
   @override
-  State<NewTransaction> createState() => _NewTransactionState();
+  State<NewTransaction> createState() {
+    print('CreateState NewTransaction Widget');
+    return _NewTransactionState();
+  }
 }
 
 class _NewTransactionState extends State<NewTransaction> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime _selectedDate;
+
+  _NewTransactionState(){
+    print('CreateState NewTransaction State ');
+  }
+
+  @override
+  void initState() {
+    print('initState()');
+    super.initState();
+  }
+  //
+
+  @override
+  void didUpdateWidget(covariant NewTransaction oldWidget) {
+    print('didUpdateWidget()');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    print('dispose()');
+    super.dispose();
+  }
 
   void _submitData() {
     if (_amountController.text.isEmpty) {
